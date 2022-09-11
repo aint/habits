f27a7ca3bc8a38b4c9ec2a5165b5304aac5f3b63
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2022-09-11 13:45:19 +0300

    Init project

---
+++ Habits/HabitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitsApp.swift
+//  Habits
+//
+//  Created by Oleksandr T on 10.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

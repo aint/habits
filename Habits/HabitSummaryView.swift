4fefb6d208b8be8e90c3e438fdbca89569bb3795
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2023-01-09 21:01:19 +0200

    Display habit status for past days

--- Habits/HabitSummaryView.swift
+++ Habits/HabitSummaryView.swift
@@ -1,29 +1,44 @@
 import SwiftUI
 
 struct HabitSummaryView: View {
-    let habit: Habit
+    private static let positiveIcon = "checkmark"
+    private static let negativeIcon = "multiply"
+
+    @Binding var habit: Habit
     
     var body: some View {
         HStack {
             Label("\(habit.name)", systemImage: "circle.dashed")
-            //  .labelStyle(.trailingIcon)
                 .font(.headline)
             Spacer()
-            Image(systemName: "checkmark")
-            Image(systemName: "checkmark")
-            Image(systemName: "multiply")
-            Image(systemName: "multiply")
-            Image(systemName: "checkmark")
+            ForEach(Date.past(4, .day), id: \.self) { date in
+                Image(systemName: getIconOrDefault(date))
+                    .padding(.leading)
+                    .onTapGesture {
+                        if habit.checkEntry(date: date) {
+                            habit.deleteEntry(date: date)
+                            return
+                        }
+                        habit.addEntry(date: date)
+                    }
+            }
         }
         .font(.body)
         .foregroundColor(habit.uiColor)
-        .padding([.top, .bottom, .trailing])
+//        .padding([.trailing])
+    }
+
+    func getIconOrDefault(_ date: Date) -> String {
+        if habit.checkEntry(date: date) {
+            return HabitSummaryView.positiveIcon
+        }
+        return HabitSummaryView.negativeIcon
     }
 }
 
 struct HabitSummaryView_Previews: PreviewProvider {
     static var previews: some View {
-        HabitSummaryView(habit: Habit.sampleData[0])
+        HabitSummaryView(habit: .constant(Habit.sampleData[0]))
             .previewLayout(.fixed(width: 400, height: 60))
     }
 }

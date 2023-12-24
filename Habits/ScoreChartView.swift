aaafa107863fd024d36b134b5949cf8fc9ffe65e
Oleksandr Tyshkovets <olexandr.tyshkovets@gmail.com>
2023-12-24 22:15:39 +0200

    Use new render func in score chart

--- Habits/ScoreChartView.swift
+++ Habits/ScoreChartView.swift
@@ -2,23 +2,44 @@ import SwiftUI
 import Charts
 
 struct ScoreChartView: View {
+    let habit: Habit
+
+    @State private var selectionGrouping = ChartGrouping.week
 
     var body: some View {
-        Chart(ChartEntry.last12Months()) {
-            LineMark(
-                x: .value("Period", $0.label),
-                y: .value("Score", $0.value)
-            )
-            .symbol(.circle)
+        VStack{
+            Picker("", selection: $selectionGrouping) {
+                ForEach(ChartGrouping.allCases, id: \.self) { grouping in
+                    Text(grouping.info.text)
+                }
+            }
+            Chart(ChartEntry.renderAll(habit.entries, selectionGrouping, .score)) {
+                LineMark(
+                    x: .value("Period", $0.label),
+                    y: .value("Score", $0.value)
+                )
+                .symbol(.circle)
+            }
+            .padding(.top)
+            .multilineTextAlignment(.center)
+            .chartYScale(domain: ClosedRange(uncheckedBounds: (lower: 0, upper: 100)))
+            .chartXAxis() {
+                AxisMarks { value in
+                    let strValue = value.as(String.self)!
+                    if strValue.contains("Q") {
+                        let index = strValue.index(strValue.startIndex, offsetBy: 4) // to avoid duplication, the quarter starts with the year, so we need to remove the first 4 chars
+                        AxisValueLabel { Text(strValue[index...]) }
+                    } else {
+                        AxisValueLabel { Text(strValue) }
+                    }
+                }
+            }
         }
-        .padding(.top)
-        .multilineTextAlignment(.center)
-        .chartYScale(domain: ClosedRange(uncheckedBounds: (lower: 0, upper: 100)))
     }
 }
 
 struct ScoreChartView_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreChartView()
+        ScoreChartView(habit: Habit.sampleData[0])
     }
 }
